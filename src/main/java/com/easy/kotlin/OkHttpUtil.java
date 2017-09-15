81d4012b0c27f4f829602d411e5e83865835c695
jack <guangjian.cgj@alibaba-inc.com>
2017-09-16 02:29:41 +0800

    fun get(url: String): String? {     var result: String? = ""     val okhttp = OkHttpClient.Builder()             .connectTimeout(10, TimeUnit.MINUTES)             .readTimeout(10, TimeUnit.MINUTES)             .writeTimeout(10, TimeUnit.MINUTES)             .build()
        val request = Request.Builder()
                .url(url)
                .addHeader("Connection", "close")
                .build()
    
        val call = okhttp.newCall(request)
    
        try {
            val response = call.execute()
            result = response.body()?.string()
            println(result)
    
        } catch (e: IOException) {
            e.printStackTrace()
        }
    
        return result
    }
    
    fun getAsync(url: String) {
        val okhttp = OkHttpClient.Builder()
                .connectTimeout(10, TimeUnit.MINUTES)
                .readTimeout(10, TimeUnit.MINUTES)
                .writeTimeout(10, TimeUnit.MINUTES)
                .build()
    
        val request = Request.Builder()
                .url(url)
                .build()
    
        val call = okhttp.newCall(request)
    
        call.enqueue(object : Callback {
            override fun onFailure(call: Call, e: IOException) {
                println(e.message)
                e.printStackTrace()
            }
    
            override fun onResponse(call: Call, response: Response) {
                try {
                    val result = response.body()?.string()
                    println(result)
                } catch (e: IOException) {
                    println("response = ${response}")
                    e.printStackTrace()
                }
            }
        })
    
    }

---
+++ src/main/java/com/easy/kotlin/OkHttpUtil.java
@@ -0,0 +1,43 @@
+package com.easy.kotlin;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class OkHttpUtil {
+
+    public static void getAsync(String url) {
+        OkHttpClient okhttp = new OkHttpClient.Builder()
+            .connectTimeout(10, TimeUnit.MINUTES)
+            .readTimeout(10, TimeUnit.MINUTES)
+            .writeTimeout(10, TimeUnit.MINUTES)
+            .build();
+
+        Request request = new Request.Builder()
+            .url(url)
+            .build();
+
+        Call call = okhttp.newCall(request);
+
+        call.enqueue(new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                // TODO
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                // TODO
+
+            }
+        });
+
+    }
+
+}
